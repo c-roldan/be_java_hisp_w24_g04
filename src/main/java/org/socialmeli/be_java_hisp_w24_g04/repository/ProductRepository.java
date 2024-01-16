0116e6a9db1119a4559566f23f3b9379caf59d98
Jorge Guerra <jorge.guerraguzman@mercadolibre.cl>
2024-01-16 12:04:49 -0300

    [feat]: loading data in entities from json files
    - Enhancement on arrow functions in update methods
    - Fix to load data correctly from constructors

--- src/main/java/org/socialmeli/be_java_hisp_w24_g04/repository/ProductRepository.java
+++ src/main/java/org/socialmeli/be_java_hisp_w24_g04/repository/ProductRepository.java
@@ -1,8 +1,14 @@
 package org.socialmeli.be_java_hisp_w24_g04.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.socialmeli.be_java_hisp_w24_g04.model.Product;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,13 +16,25 @@ import java.util.Optional;
 @Repository
 public class ProductRepository implements IProductRepository {
     private List<Product> productRepository;
+    private String jsonFile = "classpath:data/products.json";
 
-    public void setProductRepository() {
+    public ProductRepository() {
         this.productRepository = loadProducts();
     }
 
     private ArrayList<Product> loadProducts() {
-        return new ArrayList<>();
+        ArrayList<Product> data = null;
+        File file;
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        TypeReference<ArrayList<Product>> typeRef = new TypeReference<>() {};
+        try {
+            file = ResourceUtils.getFile(this.jsonFile);
+            data = objectMapper.readValue(file, typeRef);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 
     @Override
@@ -62,12 +80,7 @@ public class ProductRepository implements IProductRepository {
     public Product update(Product entity) {
         productRepository = productRepository
                 .stream()
-                .map(product -> {
-                    if (product.getProductId().equals(entity.getProductId()))
-                        return entity;
-                    else
-                        return product;
-                }).toList();
+                .map(product -> product.getProductId().equals(entity.getProductId()) ? entity : product).toList();
 
         return entity;
     }
