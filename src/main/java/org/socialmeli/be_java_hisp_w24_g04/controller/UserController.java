dce8274dc2cd467d6cb4fce64d96f1f7fb941ab2
Jorge Guerra <jorge.guerraguzman@mercadolibre.cl>
2024-01-16 12:57:29 -0300

    [feat]: List of sellers with followed list

--- src/main/java/org/socialmeli/be_java_hisp_w24_g04/controller/UserController.java
+++ src/main/java/org/socialmeli/be_java_hisp_w24_g04/controller/UserController.java
@@ -1,5 +1,8 @@
 package org.socialmeli.be_java_hisp_w24_g04.controller;
 
+import org.socialmeli.be_java_hisp_w24_g04.dto.UserFollowedDTO;
+import org.socialmeli.be_java_hisp_w24_g04.exception.NotFoundException;
+import org.socialmeli.be_java_hisp_w24_g04.model.User;
 import org.socialmeli.be_java_hisp_w24_g04.repository.IUserRepository;
 import org.socialmeli.be_java_hisp_w24_g04.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -17,8 +22,11 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    //@GetMapping("/users/{userId}/followed/list")
-    //public ResponseEntity<?> userFollowedList(@PathVariable int userId){
-
-    //}
+    @GetMapping("/users/{userId}/followed/list")
+    public ResponseEntity<UserFollowedDTO> userFollowedList(@PathVariable int userId){
+        Optional<User> user= userRepository.findById(userId);
+        if(user.isEmpty())
+            throw new NotFoundException("El usuario no existe");
+        return ResponseEntity.ok(userRepository.getUserFollowedDTO(user.get()));
+    }
 }
--- src/main/java/org/socialmeli/be_java_hisp_w24_g04/service/IUserService.java
+++ src/main/java/org/socialmeli/be_java_hisp_w24_g04/service/IUserService.java
@@ -1,4 +1,11 @@
 package org.socialmeli.be_java_hisp_w24_g04.service;
 
+import org.socialmeli.be_java_hisp_w24_g04.dto.UserFollowedDTO;
+import org.socialmeli.be_java_hisp_w24_g04.model.User;
+
+import java.util.Optional;
+
 public interface IUserService {
+    public Optional<User> findById(int id);
+    public UserFollowedDTO getUserFollowedDTO(User user);
 }
