ada0508f84e69c9ebe1b5e1ab64feba83f6c6321
FacuFM <facundo.fmamaniflores@mercadolibre.com>
2024-01-24 16:19:33 -0300

    feat: added T-0003

--- src/test/java/org/socialmeli/be_java_hisp_w24_g04/unittest/service/UserServiceTests.java
+++ src/test/java/org/socialmeli/be_java_hisp_w24_g04/unittest/service/UserServiceTests.java
@@ -1,11 +1,22 @@
 package org.socialmeli.be_java_hisp_w24_g04.unittest.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.socialmeli.be_java_hisp_w24_g04.dto.UserDTO;
+import org.socialmeli.be_java_hisp_w24_g04.exception.BadRequestException;
 import org.socialmeli.be_java_hisp_w24_g04.repository.UserRepository;
+import org.socialmeli.be_java_hisp_w24_g04.service.IUserService;
 import org.socialmeli.be_java_hisp_w24_g04.service.UserService;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
@@ -15,4 +26,29 @@ public class UserServiceTests {
 
     @InjectMocks
     UserService userService;
+
+    private Set<UserDTO> users;
+
+    @BeforeEach
+    public void setUp() {
+        users = new HashSet<>(Arrays.asList(
+                new UserDTO(101, "User1"),
+                new UserDTO(103, "User3"),
+                new UserDTO(102, "User2")
+        ));
+    }
+
+    @Test
+    public void orderByNameHappyPathTest() {
+        Assertions.assertDoesNotThrow(() -> IUserService.orderList("name_asc", users));
+        Assertions.assertDoesNotThrow(() -> IUserService.orderList("name_desc", users));
+    }
+
+    @Test
+    public void orderByNameSadathTest() {
+        Assertions.assertThrows(BadRequestException.class, () -> IUserService.orderList("nameAsc", users));
+        Assertions.assertThrows(BadRequestException.class, () -> IUserService.orderList("id_asc", users));
+        Assertions.assertThrows(BadRequestException.class, () -> IUserService.orderList("name_a", users));
+    }
+
 }
