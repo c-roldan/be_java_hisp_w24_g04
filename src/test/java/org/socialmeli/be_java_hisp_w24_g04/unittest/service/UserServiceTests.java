8d9d5c10e8c66152a991b21d577e97191ee5b89f
Nicolas Ortega <nicolas.ortegacaicedo@mercadolibre.com.co>
2024-01-24 14:48:03 -0500

    test(service): added unit test for getFollowersCount feature

--- src/test/java/org/socialmeli/be_java_hisp_w24_g04/unittest/service/UserServiceTests.java
+++ src/test/java/org/socialmeli/be_java_hisp_w24_g04/unittest/service/UserServiceTests.java
@@ -1,11 +1,19 @@
 package org.socialmeli.be_java_hisp_w24_g04.unittest.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.socialmeli.be_java_hisp_w24_g04.dto.UserDTO;
+import org.socialmeli.be_java_hisp_w24_g04.model.User;
 import org.socialmeli.be_java_hisp_w24_g04.repository.UserRepository;
 import org.socialmeli.be_java_hisp_w24_g04.service.UserService;
+
+import java.util.Optional;
+import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
@@ -15,4 +23,26 @@ public class UserServiceTests {
 
     @InjectMocks
     UserService userService;
+
+    @Test
+    public void getFollowersCountTest() {
+        // arrange
+        Set<UserDTO> followers = Set.of(
+                new UserDTO(2, "user2"),
+                new UserDTO(3, "user3"),
+                new UserDTO(4, "user4")
+        );
+
+        User user = new User();
+        user.setUserId(1);
+        user.setFollowers(followers);
+
+        Mockito.when(userRepository.get(user.getUserId())).thenReturn(Optional.of(user));
+
+        // act
+        Integer followersCount = userService.findById(user.getUserId()).getFollowers().size();
+
+        // assert
+        Assertions.assertEquals(3, followersCount);
+    }
 }
